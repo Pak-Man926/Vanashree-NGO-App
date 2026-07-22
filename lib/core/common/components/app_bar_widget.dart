e8c03f9ed3314f5f1178fd93f552d6711fa5f00e
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-07-22 23:32:38 +0300

    Appbar redesign to have a seamless view of the colors in the scaffold

--- lib/core/common/components/app_bar_widget.dart
+++ lib/core/common/components/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../extensions/build_context_extensions.dart';
 import 'app_back_button.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
@@ -9,18 +10,24 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
     this.showBackButton = false,
     this.centerTitle = true,
     this.actions = const [],
+    this.backgroundColor,
   });
   final String title;
   final bool showBackButton, centerTitle;
   final List<Widget> actions;
+  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(title),
+      title: Text(
+        title,
+        style: TextStyle(color: context.theme.colorScheme.primary),
+      ),
       centerTitle: centerTitle,
       leading: showBackButton ? const AppBackButton() : null,
       actions: actions,
+      backgroundColor: backgroundColor,
     );
   }
 
