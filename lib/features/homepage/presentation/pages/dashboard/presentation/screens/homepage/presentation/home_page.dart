3ad21e5d355199981650eba27afb5cd7d8cda8df
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-07-28 17:14:52 +0300

    Intergration of the widgets but still awaits completion

--- lib/features/homepage/presentation/pages/dashboard/presentation/screens/homepage/presentation/home_page.dart
+++ lib/features/homepage/presentation/pages/dashboard/presentation/screens/homepage/presentation/home_page.dart
@@ -1,12 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_icons/phosphor_icons.dart';
+import 'package:vanashree_ngo_application/core/common/components/primary_button.dart';
+import 'package:vanashree_ngo_application/features/homepage/presentation/pages/dashboard/presentation/screens/homepage/presentation/widgets/plant_info_container.dart';
+import 'package:vanashree_ngo_application/features/homepage/presentation/pages/dashboard/presentation/screens/homepage/presentation/widgets/status_info_container.dart';
+
+import '../../../../../../../../../core/common/constants/sizedbox_constants.dart';
+import '../../../../../../../../../core/extensions/build_context_extensions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Home"),)
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 15.0, top: 10.0),
+          child: Column(
+            mainAxisAlignment: .start,
+            crossAxisAlignment: .start,
+            children: [
+              Text(
+                "THE LIVING ARCHIVE",
+                //textAlign: TextAlign.left,
+                style: context.textTheme.titleMedium!.copyWith(
+                  color: context.theme.colorScheme.tertiaryContainer,
+                ),
+              ),
+              const Spacing.vertical(5),
+              Text("My Plants", style: context.textTheme.displayMedium),
+              const Spacing.vertical(5),
+              Text(
+                "Monitoring your curated collection of native seedlings and their ecological progress.",
+                style: context.textTheme.bodyLarge,
+              ),
+              const Spacing.vertical(15),
+              const Row(
+                children: [
+                  // Left Card
+                  Expanded(
+                    child: EnvironmentalStatsRow(
+                      title: 'ACTIVE GROWTH',
+                      value: '12',
+                      isHighlighted: false,
+                    ),
+                  ),
+
+                  Spacing.horizontal(12), // Gap between cards
+                  // Right Card
+                  Expanded(
+                    child: EnvironmentalStatsRow(
+                      title: 'CARBON OFFSET',
+                      value: '4.2kg',
+                      isHighlighted: true, // Turns on the left border
+                    ),
+                  ),
+                ],
+              ),
+              const Spacing.vertical(30),
+              const PlantInfoContainer(
+                heading: "Green Sentinel",
+                subheading: "Neem(Azadirachta indica)",
+                details: "Planted by Ananya Rao",
+              ),
+              const Spacing.vertical(10),
+              const PlantInfoContainer(
+                heading: "River Guardian",
+                subheading: "Bamboo (Bambusoideae)",
+                details: "Planted by Rahul Mahra",
+              ),
+              const Spacing.vertical(10),
+              const PlantInfoContainer(
+                heading: "Shadow Bloom",
+                subheading: "Peepal(Ficus religiosa)",
+                details: "Planted by You",
+              ),
+              const Spacing.vertical(10),
+              const PlantInfoContainer(
+                heading: "Silent Oak",
+                subheading: 'Indian Oak (Barringtonia acutangula)',
+                details: "Planted by Surash K.",
+              ),
+              const Spacing.vertical(20),
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: context.colorScheme.onPrimary,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: .center,
+                    children: [
+                      PhosphorIcon(PhosphorIcons.pottedPlant(), size: 48),
+                      const Spacing.vertical(5),
+                      Text(
+                        "Expand Your Archive",
+                        style: context.textTheme.headlineSmall!.copyWith(
+                          color: context.colorScheme.primary,
+                        ),
+                      ),
+                      const Spacing.vertical(5),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 60.0, right: 20),
+                        child: Text(
+                          "Adding new native species increases local biodiversity. Request a new seedling today.",
+                          style: context.textTheme.bodySmall,
+                        ),
+                      ),
+                      const Spacing.vertical(10),
+                      Container(
+                        width: 160,
+                        decoration: BoxDecoration(
+                        color: context.colorScheme.primary,
+                          borderRadius: BorderRadius.circular(15)),
+                        child: MaterialButton(
+                         color: context.colorScheme.primary,
+                          textColor: context.colorScheme.surfaceBright,
+                          child: const Text("Request Seedlings"),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacing.vertical(90),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
