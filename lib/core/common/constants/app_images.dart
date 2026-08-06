6bb3ea351c69d1b7e48eb9e360210499875d8e11
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-08-06 16:46:55 +0300

    Additional placeholder assets

--- lib/core/common/constants/app_images.dart
+++ lib/core/common/constants/app_images.dart
@@ -2,4 +2,8 @@ class AppImages {
   static const String basePath = 'assets/images/';
   static const String seedling = '${basePath}seedling.jpg';
   static const String plant = '${basePath}plant.svg';
+  static const String tree = '${basePath}trees.jpg';
+  static const String flower = '${basePath}flower.jpg';
+  static const String avatar1 = "${basePath}random_avatar.png";
+  static const String avatar2 = "${basePath}lego_avatar.png";
 }
